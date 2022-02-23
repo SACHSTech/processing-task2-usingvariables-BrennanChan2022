3a2f83cc2f0f0ce4a796e314f069dff98ea43e25
Brennan Chan <99412514+BrennanChan2022@users.noreply.github.com>
2022-02-23 14:22:52 +0000

    copy

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,66 @@
 import processing.core.PApplet;
 
+  /**
+  * A program that draws a house with a background.
+  * @author: B. Chan
+  */
+
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  //Background
   public void setup() {
-    background(210, 255, 173);
+    background(176, 227, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //Draw Grass + Dirt
+    noStroke();
+    fill(57, 184, 65);
+    rect(0,400,500,50);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(107, 68, 27);
+    rect(0,450,500,50);
+ 
+    // Draw House + Roof
+    stroke(0,0,0);
+    strokeWeight(1);
+    fill(148, 61, 61);
+    rect(130,160,240,240);
+
+    fill(130, 127, 127);
+    rect(310,80,40,60);
+    triangle(100, 160, 250, 80, 400, 160);
+
+    // Draw Door and Windows
+    fill(79, 5, 5);
+    rect(220,300,60,100);
+    fill(110, 0, 0);
+    ellipse(270, 350, 10, 10);
+
+    fill(203, 212, 211);
+    ellipse(180, 270, 50, 60);
+    ellipse(320, 270, 50, 60);
+    rect(220,180,60,50);
+
+    stroke(0,0,0);
+    line(250, 180, 250, 230); 
+    line(220, 205, 280, 205); 
+
+    //Draw Sun and Clouds
+    noStroke();
+    fill(255, 251, 33);
+    ellipse(40, 40, 60, 60);
+
+    fill(255, 255, 255);
+    ellipse(150, 40, 110, 45);
+    ellipse(305, 40, 165, 50);
+    ellipse(440, 40, 85, 45);
   }
   
-  // define other methods down here.
 }
